c8043af17806d4bdeff113d37d5fd6e24badfa79
Arnav-Rawat <77447759+Arnav-Rawat@users.noreply.github.com>
2022-01-09 16:46:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,6 +14,20 @@ function setup(){
 
 function draw(){
     image(video,0,0,480,380);
+    if (status != ""){
+        objectDetector.detect(video,gotResult);
+        for (i=0; i< object.length; i++)
+        {
+            document.getElementById("status").innerHTML= "status : object detected";
+            document.getElementById("number_of_objects").innerHTML = "number_of_objects detected are : " + objects.length;
+            fill("#FF0000");
+            percentage = floor(object[i].confidence * 100);
+            text(object[i].label + " "+ percentage+ "%",objects[i].x + 15, objects[i].y + 15);
+            nofill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
     
 }
 
